6f11eb9eaf5b16c875949904eea8ef86854fbfe5
Hyy <huyh2k4@gmail.com>
2025-05-16 11:35:38 +0700

    Add files via upload

---
+++ Buoi1/ex02_01.py
@@ -0,0 +1,4 @@
+ten =input("Nhập tên của bạn: ") 
+tuoi =input("Nhập tuổi của bạn: ") 
+# In thông điệp chào mừng với thông tin vừa nhập 
+print("Chào mừng,", ten, "! Bạn", tuoi, "tuối.")
---
+++ Buoi1/lab01.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print("My name is vhuyy") 
+print("HUTECH University") 
